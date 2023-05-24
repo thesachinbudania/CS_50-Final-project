a2dc271874c8462ea84ecd4f2ebe6970942db6a1
Sachin Budania <thesachinbudania@gmail.com>
2023-05-25 01:41:06 +0530

    designed the home page

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, render_template
  
 app = Flask(__name__)
 
+if __name__ == '__main__':
+    app.run(debug=True)
+
 @app.route("/")
 def main():
-    return render_template("layout.html")
+    return render_template("home.html")
